3a5e318c6d1fc94cca8788b05d956ccad39db611
bbadzioch <badzioch@buffalo.edu>
2020-07-24 23:19:53 -0400

    rebuild

--- source/conf.py
+++ source/conf.py
@@ -134,3 +134,6 @@ html_show_copyright = True
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 html_last_updated_fmt = '%b %d, %Y'
+
+# favicon
+html_favicon = "_static/favicon.ico"
